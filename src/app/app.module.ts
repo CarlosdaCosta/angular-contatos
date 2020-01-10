cf5fbc5e9d07994f750382554d0b68911b8da712
Carlos Eduardo <129226@upf.br>
2020-01-10 13:36:14 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HeaderComponent } from './features/header/header.component';
+import { SearchComponent } from './features/search/search.component';
+import { ContactListComponent } from './features/contact-list/contact-list.component';
+import { ContactFormComponent } from './features/contact-form/contact-form.component';
+import { ContactBoxComponent } from './features/contact-box/contact-box.component';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		DetailComponent,
+		HeaderComponent,
+		SearchComponent,
+		ContactListComponent,
+		ContactFormComponent,
+		ContactBoxComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatIconModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatListModule,
+		MatCardModule,
+		MatDialogModule,
+		MatInputModule,
+		MatFormFieldModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/features/contact-box/contact-box.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Contact } from 'src/app/model/Contact';
+
+@Component({
+  selector: 'app-contact-box',
+  templateUrl: './contact-box.component.html',
+  styleUrls:['./contact-box.component.scss']
+  
+})
+
+export class ContactBoxComponent implements OnInit {
+  @Input() contact: Contact
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
